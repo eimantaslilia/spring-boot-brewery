4442fa1460af0c22f7c77c303677527e9e1fb32d
eimantaslilia <eimantaslilia@gmail.com>
2019-09-22 14:56:24 +0100

    initial commit

---
+++ src/test/java/com/springframework/brewery/SpringBootTestingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springframework.brewery;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootTestingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
